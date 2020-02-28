85479f3a5bfe1a61f6c44954694a0b3dba842e81
Evan Bacon <baconbrix@gmail.com>
2020-02-27 18:07:59 -0800

    Created expo button

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { TouchBarProvider, Button, TouchBar } from 'react-touchbar-electron';
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
+      <TouchBar>
+        <Button icon={'./assets/touch-bar-icon.png'} onClick={() => {
+          alert('Deploying to Expo')
+        }} />
+      </TouchBar>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
 }
 
+export default () => <TouchBarProvider><App /></TouchBarProvider>
 const styles = StyleSheet.create({
   container: {
     flex: 1,
